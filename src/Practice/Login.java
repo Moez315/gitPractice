6787c25b156b83869046cf396a65180b22e326a1
mihrya <mihrigul1208@gmail.com>
2020-08-06 18:14:54 -0400

    added git practice

--- src/Practice/Login.java
+++ src/Practice/Login.java
@@ -6,7 +6,20 @@ public class Login {
 		System.out.println("Hello git");
 		
 		// i need to add git hub
+	
+	/*
+	 * 08/05/2020
+Practice Topic: Basic Commands
+    pwd: print working directory
+    ls: lists everything under the current directory
+        dir (for windows)
+    cd: change directory
+    cd ..: goes back to previous directory
+    rm -r: removes
+
+	 */
 	}
+	
 	
 
 }
